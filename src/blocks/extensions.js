c7ed172b6cabd03f83f0a2ef3336c4a8b6f67ef7
koen1711 <za4266468@gmail.com>
2023-09-24 17:29:48 +0200

    fix: add LOGIC_COMPARE_EXTENSION

--- src/blocks/extensions.js
+++ src/blocks/extensions.js
@@ -348,4 +348,67 @@ const WHILE_UNTIL_TOOLTIPS = {
 	'UNTIL': '%{BKY_CONTROLS_WHILEUNTIL_TOOLTIP_UNTIL}',
 };
 
-export {TEXT_QUOTES_EXTENSION, APPEND_STATEMENT_INPUT_STACK, CONTROLS_IF_MUTATOR_MIXIN, CONTROLS_IF_TOOLTIP_EXTENSION, WHILE_UNTIL_TOOLTIPS};
+/**
+ * Adds dynamic type validation for the left and right sides of a logic_compare
+ * block.
+ * @mixin
+ * @augments Block
+ * @readonly
+ */
+const LOGIC_COMPARE_ONCHANGE_MIXIN = {
+	/**
+	 * Called whenever anything on the workspace changes.
+	 * Prevent mismatched types from being compared.
+	 * @param {!AbstractEvent} e Change event.
+	 * @this {Block}
+	 */
+	onchange: function(e) {
+		if (!this.prevBlocks_) {
+			this.prevBlocks_ = [null, null];
+		}
+		
+		const blockA = this.getInputTargetBlock('A');
+		const blockB = this.getInputTargetBlock('B');
+		// Disconnect blocks that existed prior to this change if they don't match.
+		if (blockA && blockB &&
+			!this.workspace.connectionChecker.doTypeChecks(
+				blockA.outputConnection, blockB.outputConnection)) {
+			// Mismatch between two inputs.  Revert the block connections,
+			// bumping away the newly connected block(s).
+			Blockly.Events.setGroup(e.group);
+			const prevA = this.prevBlocks_[0];
+			if (prevA !== blockA) {
+				blockA.unplug();
+				if (prevA && !prevA.isDisposed() && !prevA.isShadow()) {
+					// The shadow block is automatically replaced during unplug().
+					this.getInput('A').connection.connect(prevA.outputConnection);
+				}
+			}
+			const prevB = this.prevBlocks_[1];
+			if (prevB !== blockB) {
+				blockB.unplug();
+				if (prevB && !prevB.isDisposed() && !prevB.isShadow()) {
+					// The shadow block is automatically replaced during unplug().
+					this.getInput('B').connection.connect(prevB.outputConnection);
+				}
+			}
+			this.bumpNeighbours();
+			Blockly.Events.setGroup(false);
+		}
+		this.prevBlocks_[0] = this.getInputTargetBlock('A');
+		this.prevBlocks_[1] = this.getInputTargetBlock('B');
+	},
+};
+
+/**
+ * "logic_compare" extension function. Adds type left and right side type
+ * checking to "logic_compare" blocks.
+ * @this {Block}
+ * @readonly
+ */
+const LOGIC_COMPARE_EXTENSION = function() {
+	// Add onchange handler to ensure types are compatible.
+	this.mixin(LOGIC_COMPARE_ONCHANGE_MIXIN);
+};
+
+export {TEXT_QUOTES_EXTENSION, APPEND_STATEMENT_INPUT_STACK, CONTROLS_IF_MUTATOR_MIXIN, CONTROLS_IF_TOOLTIP_EXTENSION, WHILE_UNTIL_TOOLTIPS, LOGIC_COMPARE_EXTENSION};
