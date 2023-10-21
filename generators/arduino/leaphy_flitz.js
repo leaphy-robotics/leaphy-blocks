6151918c04396058029d6b070691bfc8dd7a4c4b
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2023-10-21 16:53:59 +0200

    chore: add flitz nano blocks

--- generators/arduino/leaphy_flitz.js
+++ generators/arduino/leaphy_flitz.js
@@ -11,23 +11,42 @@ function getCodeGenerators(Arduino) {
             setup = 'pinMode(8, INPUT);\n  pinMode(9, OUTPUT);\n  pinMode(10, OUTPUT);\n  digitalWrite(8, LOW);\n  digitalWrite(9, HIGH);\n'
             code = 'digitalRead(8)'
         }
-        Arduino.setups_['setup_flitz_stomach'] = setup;
+        Arduino.addSetup('setup_flitz_stomach', setup, false);
+        return [code, Arduino.ORDER_ATOMIC];
+    };
+
+    Arduino.forBlock['leaphy_flitz_nano_read_stomach_sensor'] = function (block){
+        let setup = 'pinMode(2, INPUT);'
+        let code = 'digitalRead(2)'
+        Arduino.addSetup('setup_flitz_stomach', setup, false);
         return [code, Arduino.ORDER_ATOMIC];
     };
 
     Arduino.forBlock['leaphy_flitz_read_hand_sensor'] = function (block){
-        Arduino.setups_['setup_flitz_stomach'] = 'pinMode(14, OUTPUT);\n pinMode(15, OUTPUT);\n pinMode(2, INPUT);\n digitalWrite(14, HIGH);\n digitalWrite(15, LOW);\n'
+        Arduino.addSetup('setup_flitz_hand', 'pinMode(14, OUTPUT);\n pinMode(15, OUTPUT);\n pinMode(2, INPUT);\n digitalWrite(14, HIGH);\n digitalWrite(15, LOW);\n', false);
         var code = 'analogRead(2)'
         return [code, Arduino.ORDER_ATOMIC];
     };
 
+    Arduino.forBlock['leaphy_flitz_nano_read_hand_sensor'] = function (block){
+        Arduino.addSetup('setup_flitz_hand', 'pinMode(A0, INPUT);', false);
+        var code = 'analogRead(A0)'
+        return [code, Arduino.ORDER_ATOMIC];
+    };
+
     Arduino.forBlock['leaphy_flitz_led'] = function (block){
-        Arduino.setups_['setup_flitz_led'] = ''
         var flitz_red = Arduino.valueToCode(this, 'FLITZ_LED_R', Arduino.ORDER_ATOMIC) || '0'
         var flitz_green = Arduino.valueToCode(this, 'FLITZ_LED_G', Arduino.ORDER_ATOMIC) || '0'
         var flitz_blue = Arduino.valueToCode(this, 'FLITZ_LED_B', Arduino.ORDER_ATOMIC) || '0'
-        //var code = 'setLedd(' + flitz_red + ', ' + flitz_green + ', ' + flitz_blue + ');\n';
         var code = 'analogWrite(3, ' + flitz_red + ');\nanalogWrite(5, ' + flitz_green + ');\nanalogWrite(6, ' + flitz_blue + ');\n';
+        return code;
+    };
+
+    Arduino.forBlock['leaphy_flitz_nano_led'] = function (block){
+        var flitz_red = Arduino.valueToCode(this, 'FLITZ_LED_R', Arduino.ORDER_ATOMIC) || '0'
+        var flitz_green = Arduino.valueToCode(this, 'FLITZ_LED_G', Arduino.ORDER_ATOMIC) || '0'
+        var flitz_blue = Arduino.valueToCode(this, 'FLITZ_LED_B', Arduino.ORDER_ATOMIC) || '0'
+        var code = 'analogWrite(9, ' + flitz_red + ');\nanalogWrite(10, ' + flitz_green + ');\nanalogWrite(11, ' + flitz_blue + ');\n';
         return code;
     };
 }
