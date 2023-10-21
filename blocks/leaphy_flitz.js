769e7bd36a9f1b63bb140777debeeb6103a15444
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2023-10-21 16:56:04 +0200

    chore: add flitz nano blocks (#7)

--- blocks/leaphy_flitz.js
+++ blocks/leaphy_flitz.js
@@ -1,16 +1,8 @@
 /**
- * @license
- * Copyright 2012 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
  * @fileoverview Blocks for the Leaphy Flitz robot.
  */
 'use strict';
 
-// const {BlockDefinition} = goog.requireType('Blockly.blocks');
-// TODO (6248): Properly import the BlockDefinition type.
 /* eslint-disable-next-line no-unused-vars */
 
 const stomachSensorOptions =
@@ -38,6 +30,14 @@ const blocks = [
     'helpUrl': '',
   },
   {
+    'type': 'leaphy_flitz_nano_read_stomach_sensor',
+    'message0': '%%{BKY_LEAPHY_READ_STOMACH}',
+    'output': 'Number',
+    'style': 'leaphy_blocks',
+    'tooltip': '',
+    'helpUrl': '',
+  },
+  {
     'type': 'leaphy_flitz_read_hand_sensor',
     'message0': '%%{BKY_LEAPHY_READ_HAND}',
     'output': 'Number',
@@ -46,7 +46,32 @@ const blocks = [
     'helpUrl': '',
   },
   {
+    'type': 'leaphy_flitz_nano_read_hand_sensor',
+    'message0': '%%{BKY_LEAPHY_READ_HAND}',
+    'output': 'Number',
+    'style': 'leaphy_blocks',
+    'tooltip': '',
+    'helpUrl': '',
+  },
+  {
     'type': 'leaphy_flitz_led',
+    'message0':
+      '%%{BKY_LEAPHY_FLITZ_LED} %1 %%{BKY_LEAPHY_FLITZ_LED_R} %2 %%{BKY_LEAPHY_FLITZ_LED_G} %3 %%{BKY_LEAPHY_FLITZ_LED_B} %4',
+    'args0': [
+      {'type': 'input_dummy'},
+      {'type': 'input_value', 'name': 'FLITZ_LED_R', 'check': 'Number'},
+      {'type': 'input_value', 'name': 'FLITZ_LED_G', 'check': 'Number'},
+      {'type': 'input_value', 'name': 'FLITZ_LED_B', 'check': 'Number'},
+    ],
+    'inputsInline': true,
+    'previousStatement': null,
+    'nextStatement': null,
+    'style': 'leaphy_blocks',
+    'tooltip': '',
+    'helpUrl': '',
+  },
+  {
+    'type': 'leaphy_flitz_nano_led',
     'message0':
       '%%{BKY_LEAPHY_FLITZ_LED} %1 %%{BKY_LEAPHY_FLITZ_LED_R} %2 %%{BKY_LEAPHY_FLITZ_LED_G} %3 %%{BKY_LEAPHY_FLITZ_LED_B} %4',
     'args0': [
