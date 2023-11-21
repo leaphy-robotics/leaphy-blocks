a2235902734c32259532319a07b9595162d904fd
Jefta <jeftadegraaff@gmail.com>
2023-11-21 10:02:25 +0100

    Update procedures.js

--- generators/arduino/procedures.js
+++ generators/arduino/procedures.js
@@ -44,7 +44,7 @@ function getCodeGenerators(Arduino) {
     }
 
     // Get return type
-    var returnType = "double";
+    var returnType = "void";
     //returnType = Arduino.getArduinoType_(returnType);
 
     // Construct code
