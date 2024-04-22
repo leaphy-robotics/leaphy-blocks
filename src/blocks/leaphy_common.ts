2d738ac897f1c2be59922325835d7fe5447774bc
Basie147 <basdegraaf43@hotmail.com>
2024-04-22 13:49:40 +0200

    show on sreen same line

--- src/blocks/leaphy_common.ts
+++ src/blocks/leaphy_common.ts
@@ -92,6 +92,21 @@ function getBlocks(board: Board): BlockDefinition {
         },
 
         {
+            type: "leaphy_serial_print_without_line",
+            message0: "%%{BKY_LEAPHY_SERIAL_PRINT_WITHOUT_LINE} %1 %2",
+            args0: [
+                { type: "input_dummy" },
+                { type: "input_value", name: "VALUE" },
+            ],
+            inputsInline: true,
+            previousStatement: null,
+            nextStatement: null,
+            style: "leaphy_blocks",
+            tooltip: "",
+            helpUrl: "",
+        },
+
+        {
             type: "leaphy_serial_print_value",
             message0: "%%{BKY_LEAPHY_SERIAL_PRINT} %1 %2 = %3 %4",
             args0: [
