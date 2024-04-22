715e6fc84b0b6ba3fc6d1b13b2f286e012369c94
Basie147 <basdegraaf43@hotmail.com>
2024-04-22 13:18:41 +0200

    Update leaphy_common.ts

--- src/blocks/leaphy_common.ts
+++ src/blocks/leaphy_common.ts
@@ -91,6 +91,20 @@ function getBlocks(board: Board): BlockDefinition {
             helpUrl: "",
         },
         {
+            type: "leaphy_serial_print__without_line",
+            message0: "%%{BKY_LEAPHY_SERIAL_PRINT_WITHOUT_LINE} %1 %2",
+            args0: [
+                { type: "input_dummy" },
+                { type: "input_value", name: "VALUE" },
+            ],
+            inputsInline: true,
+            previousStatement: null,
+            nextStatement: null,
+            style: "leaphy_blocks",
+            tooltip: "",
+            helpUrl: "",
+        },
+        {
             type: "leaphy_serial_print_value",
             message0: "%%{BKY_LEAPHY_SERIAL_PRINT} %1 %2 = %3 %4",
             args0: [
