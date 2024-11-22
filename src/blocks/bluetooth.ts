8fb7b189f34976a4fee4e5a0bef69fcf49cfb611
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-11-22 14:40:19 +0100

    feat: add simple Bluetooth blocks

--- src/blocks/bluetooth.ts
+++ src/blocks/bluetooth.ts
@@ -1,6 +1,34 @@
 import {BlockDefinition} from "blockly/core/blocks";
 
 const blocks: BlockDefinition = [
+    {
+        type: "bluetooth_setup",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_BLUETOOTH_SETUP}",
+        args0: [
+            {
+                type: "input_value",
+                name: "NAME",
+                check: ["String"],
+            },
+        ],
+        previousStatement: null,
+        nextStatement: null,
+    },
+    {
+        type: "bluetooth_on_connect",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_BLUETOOTH_ON_CONNECT}",
+        extensions: ["appendStatementInputStack"],
+        nextStatement: null,
+    },
+    {
+        type: "bluetooth_on_disconnect",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_BLUETOOTH_ON_DISCONNECT}",
+        extensions: ["appendStatementInputStack"],
+        nextStatement: null,
+    },
 ];
 
 export {blocks};
