374612550ac49164fe28247ac2430a56a206f34e
Maarten van Heusden <50550545+mmvanheusden@users.noreply.github.com>
2024-12-14 09:42:33 +0100

    chore: save progress

--- src/blocks/bluetooth.ts
+++ src/blocks/bluetooth.ts
@@ -23,11 +23,53 @@ const blocks: BlockDefinition = [
         nextStatement: null,
     },
     {
+        type: "bluetooth_on_characteristic_updated",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_BLUETOOTH_CHARACTERISTIC_ON_UPDATE}",
+        args0: [
+            {
+                type: "input_value",
+                name: "NAME",
+                check: ["String"],
+            },
+        ],
+        extensions: ["appendStatementInputStack"],
+        nextStatement: null,
+    },
+    {
         type: "bluetooth_on_disconnect",
         style: "bluetooth_blocks",
         message0: "%{BKY_LEAPHY_BLUETOOTH_ON_DISCONNECT}",
         extensions: ["appendStatementInputStack"],
         nextStatement: null,
+    },
+    {
+        type: "create_binary_characteristic",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_BLUETOOTH_CREATE_BINARY_CHARACTERISTIC}",
+        args0: [
+            {
+                type: "input_value",
+                name: "NAME",
+                check: ["String"],
+            },
+            { type: "input_value", name: "INITIAL_VALUE", check: "Boolean" },
+        ],
+        previousStatement: null,
+        nextStatement: null,
+    },
+    {
+        type: "bluetooth_characteristic_read",
+        style: "bluetooth_blocks",
+        message0: "%{BKY_LEAPHY_READ_BINARY_CHARACTERISTIC_VALUE}",
+        args0: [
+            {
+                type: "input_value",
+                name: "NAME",
+                check: ["String"],
+            },
+        ],
+        output: "Boolean",
     },
 ];
 
