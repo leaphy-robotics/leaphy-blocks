0b3e91e5ec6088e643cb3fbaa264b2e34ea8e799
koen1711 <koenvurk1711@gmail.com>
2025-03-11 18:20:17 +0100

    fix: missing changes that remove click

--- src/blocks/blocks.ts
+++ src/blocks/blocks.ts
@@ -3,7 +3,6 @@ import * as texts from "./text";
 import * as leaphyCommon from "./leaphy_common";
 import * as leaphyOriginal from "./leaphy_original";
 import * as leaphyFlitz from "./leaphy_flitz";
-import * as leaphyClick from "./leaphy_click";
 import * as arduino from "./arduino";
 import * as loops from "./alternatives";
 import * as mesh from "./mesh";
@@ -14,7 +13,6 @@ const blocks = [
     ...texts.blocks,
     ...leaphyOriginal.blocks,
     ...leaphyFlitz.blocks,
-    ...leaphyClick.blocks,
     ...arduino.blocks,
     ...loops.blocks,
     ...leaphyCommon.blocks,
